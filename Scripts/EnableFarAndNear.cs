510350d73a34676522e7fa1f4e014edd66abd1e5
Daniele Fundarò <daniele.fundaro@gmail.com>
2016-11-04 20:24:55 +0100

    Add new script. EnableFanAndNear 1.0
    Script to enable the function "far and near".

---
+++ Scripts/EnableFarAndNear.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap.Unity;
+
+public class EnableFarAndNear : MonoBehaviour
+{
+  public void OnTriggerEnter(Collider other)
+  {
+    FarAndNear fan = GetComponentInParent<FarAndNear>();
+
+    if (other.GetComponentInParent<IHandModel>() != null && fan != null)
+      fan.enabled = !fan.enabled;
+  }
+}
