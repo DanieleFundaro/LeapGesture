7fe562ac733b2c318e7ece1c1f25915668c160ee
Daniele Fundarò <daniele.fundaro@gmail.com>
2016-11-05 22:19:11 +0100

    Zoom 1.0
    Add new script. Now I can zoom the field of view of the main camera.

---
+++ Scripts/Zoom.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Leap.Unity;
+
+public class Zoom : MonoBehaviour
+{
+  public RigidHand manoDestra, manoSinistra;
+  public float grabAngle = 2.6f;
+  private float offset = 0;
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (manoDestra.IsTracked && manoSinistra.IsTracked)
+      if (manoDestra.GetLeapHand().GrabAngle >= grabAngle && manoSinistra.GetLeapHand().GrabAngle >= grabAngle && manoDestra.GetPalmNormal().z / manoSinistra.GetPalmNormal().z < 0 && manoDestra.GetPalmDirection().x / manoSinistra.GetPalmDirection().x < 0)
+      {
+        Camera.main.fieldOfView = (offset - Distanza());
+        SendMessageUpwards("StoAfferrando", true);
+      }
+      else
+      {
+        offset = Distanza() + Camera.main.fieldOfView;
+        SendMessageUpwards("StoAfferrando", false);
+      }
+  }
+
+  private float Distanza()
+  {
+    return (manoDestra.GetPalmPosition() - manoSinistra.GetPalmPosition()).magnitude * 100;
+  }
+}
