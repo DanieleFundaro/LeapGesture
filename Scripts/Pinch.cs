1d6a60285bc737d86a08856b09bfe174d25d5cea
Daniele Fundarò <daniele.fundaro@gmail.com>
2016-11-05 17:20:46 +0100

    Pinch 1.0
    Add new Script. I can pinch all the objects and move in the scene in all
    direction.

---
+++ Scripts/Pinch.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap.Unity;
+
+public class Pinch : MonoBehaviour
+{
+  public RigidHand mano;
+  public float minPinch = 0.9f;
+  private Collider colliso;
+  private Transform padre;
+
+  public void OnValidate()
+  {
+    IHandModel ihm = gameObject.GetComponentInParent<IHandModel>();
+
+    if (ihm != null)
+      mano = (RigidHand)ihm;
+  }
+
+  public void Start()
+  {
+    colliso = null;
+    padre = null;
+  }
+  public void OnTriggerEnter(Collider other)
+  {
+    if(colliso == null && other.tag != "Imprendibile")
+    {
+      SendMessageUpwards("StoAfferrando", true);
+      colliso = other;
+      padre = other.transform.parent;
+    }
+  }
+
+  public void OnTriggerStay(Collider other)
+  {
+    if (colliso != null)
+      mano.Pinch(colliso, padre, transform, minPinch, null);
+  }
+
+  public void OnTriggerExit(Collider other)
+  {
+    if(colliso != null)
+    {
+      mano.StopPinch(colliso, padre);
+      colliso = null;
+      padre = null;
+      SendMessageUpwards("StoAfferrando", false);
+    }
+  }
+}
