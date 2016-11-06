8cc7545e5248440e67432179ccd3d55c59537675
Daniele Fundarò <daniele.fundaro@gmail.com>
2016-11-06 15:27:05 +0100

    EnableZoom 1.0
    New script to enable the zoom gesture.

---
+++ Scripts/EnableZoom.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Leap.Unity;
+
+public class EnableZoom : MonoBehaviour
+{
+  public void OnTriggerEnter(Collider other)
+  {
+    Zoom z = GetComponentInParent<Zoom>();
+
+    if (other.GetComponentInParent<IHandModel>() != null && z != null)
+      z.enabled = !z.enabled;
+  }
+}
