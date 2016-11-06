3b3fe6145d22cf608854227cb89ae6cbbb1b5713
Daniele Fundarò <daniele.fundaro@gmail.com>
2016-11-06 04:29:39 +0100

    Zoom 1.0.1
    Now send in the right way the message (broadcast).

--- Scripts/Zoom.cs
+++ Scripts/Zoom.cs
@@ -5,26 +5,31 @@ public class Zoom : MonoBehaviour
 {
   public RigidHand manoDestra, manoSinistra;
   public float grabAngle = 2.6f;
-  private float offset = 0;
+  private float offsetZoom = 0;
 
   // Update is called once per frame
   void Update()
   {
     if (manoDestra.IsTracked && manoSinistra.IsTracked)
-      if (manoDestra.GetLeapHand().GrabAngle >= grabAngle && manoSinistra.GetLeapHand().GrabAngle >= grabAngle && manoDestra.GetPalmNormal().z / manoSinistra.GetPalmNormal().z < 0 && manoDestra.GetPalmDirection().x / manoSinistra.GetPalmDirection().x < 0)
+      if (manoDestra.GetLeapHand().GrabAngle >= grabAngle && manoSinistra.GetLeapHand().GrabAngle >= grabAngle && manoDestra.GetPalmNormal().z / manoSinistra.GetPalmNormal().z < 0 && manoDestra.GetPalmDirection().x < 0 && manoSinistra.GetPalmDirection().x > 0)
       {
-        SendMessageUpwards("InZoom", true);
-        Camera.main.fieldOfView = (offset - Distanza());
+        BroadcastMessage("InZoom", true);
+        Camera.main.fieldOfView = (offsetZoom + Distanza());
       }
       else
       {
-        offset = Distanza() + Camera.main.fieldOfView;
-        SendMessageUpwards("InZoom", false);
+        offsetZoom = Camera.main.fieldOfView - Distanza();
+        BroadcastMessage("InZoom", false);
       }
   }
 
   private float Distanza()
   {
     return (manoDestra.GetPalmPosition() - manoSinistra.GetPalmPosition()).magnitude * 100;
+  }
+
+  private Vector3 PuntoMedio()
+  {
+    return (manoDestra.GetPalmPosition() - manoSinistra.GetPalmPosition()) / 2f;
   }
 }
