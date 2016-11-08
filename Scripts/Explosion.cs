874d00f5f071885e5dd29911b3f5104306ed7042
Daniele Fundarò <daniele.fundaro@gmail.com>
2016-11-08 17:38:11 +0100

    Explosion 1.0.1
    Adapting to the extended method "Explosion" of MyRigidHand.

--- Scripts/Explosion.cs
+++ Scripts/Explosion.cs
@@ -7,8 +7,7 @@ using UnityEditor;
 public class Explosion : MonoBehaviour
 {
   public RigidHand mano;
-  private ObjectsMove om = new ObjectsMove();
-  private bool selezione = false, esplodi = false, afferraZoom = false;
+  private bool afferraZoom = false;
 
   public void OnValidate()
   {
@@ -26,46 +25,13 @@ public class Explosion : MonoBehaviour
     {
       Controller cont = new Controller();
 
-      if (cont.IsConnected && !esplodi)
+      if (cont.IsConnected)
       {
         Vector3 dir = mano.GetPalmNormal();
         dir.Normalize();
+        Ray raggio = new Ray(Vector3.MoveTowards(mano.GetPalmPosition(), dir, 0.07f), dir);
 
-        // Calcolo il raggio e distanzio il punto di inizio dal palmo della mano di 0.1, così non colpisco le dita della stessa mano
-        Ray raggio = new Ray(Vector3.MoveTowards(mano.GetPalmPosition(), dir, 0.1f), dir);
-        RaycastHit colpito = new RaycastHit();
-
-        // Disegna la linea per usarla come puntatore, così da facilitare la selezione degli oggetti
-        DrawLine(mano.GetPalmPosition(), mano.GetPalmPosition() + dir, Color.red, 0.05f);
-
-        // Controllo se è stato puntato un oggetto
-        if (Physics.Raycast(raggio, out colpito))
-          if (colpito.collider != null)
-          {
-            Transform padre = colpito.collider.transform.parent;
-
-            // Deve essere un assemblato di oggetti e non devo considerare, ovviamente, l'altra mano
-            if (padre != null && padre.tag != "Imprendibile" && padre.tag != "MainCamera")
-            {
-              RigidHand rh = (RigidHand)padre.GetComponentInParent<IHandModel>();
-
-              if (rh == null)
-              {
-                om = mano.SelectObjects(padre);
-                selezione = true;
-              }
-            }
-          }
-
-        // Controllo del gesto tipico per l'esplosione
-        esplodi = selezione && mano.ExplosionGesture();
-      }
-
-      // Parte grafica dell'esplosione. Sposto tutti gli oggetti, già selezionati, di una distanza calcolata in precedenza
-      if (esplodi)
-      {
-        esplodi = mano.PlayExplosion(om);
-        selezione = false;
+        mano.Explosion(raggio, "Imprendibile", "MainCamera");
       }
     }
   }
@@ -78,20 +44,5 @@ public class Explosion : MonoBehaviour
   private void InZoom(bool zoom)
   {
     afferraZoom = zoom;
-  }
-
-  private void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.2f)
-  {
-    GameObject myLine = new GameObject();
-    myLine.transform.position = start;
-    myLine.AddComponent<LineRenderer>();
-
-    LineRenderer lr = myLine.GetComponent<LineRenderer>();
-    lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
-    lr.SetColors(color, color);
-    lr.SetWidth(0.0025f, 0.0025f);
-    lr.SetPosition(0, start);
-    lr.SetPosition(1, end);
-    GameObject.Destroy(myLine, duration);
   }
 }
