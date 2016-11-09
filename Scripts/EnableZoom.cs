4380251b759d80cb97159b2fe3ce9de7294a2295
Daniele Fundarò <daniele.fundaro@gmail.com>
2016-11-09 03:30:15 +0100

    EnableZoom 1.1
    Now the object is activated and deactivated graphically.

--- Scripts/EnableZoom.cs
+++ Scripts/EnableZoom.cs
@@ -3,11 +3,13 @@ using Leap.Unity;
 
 public class EnableZoom : MonoBehaviour
 {
+  public void Start()
+  {
+    Utility.Enable<Zoom>(transform, null, false);
+  }
+
   public void OnTriggerEnter(Collider other)
   {
-    Zoom z = GetComponentInParent<Zoom>();
-
-    if (other.GetComponentInParent<IHandModel>() != null && z != null)
-      z.enabled = !z.enabled;
+    Utility.Enable<Zoom>(transform, other);
   }
 }
