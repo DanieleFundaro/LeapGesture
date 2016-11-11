9da28571abb8cfb37f1ed41b9015f441a8e564b3
Daniele Fundarò <daniele.fundaro@gmail.com>
2016-11-11 23:22:20 +0100

    Implosion 1.0
    New script. With the method "Implosion" of the MyRigidHand object, I can
    do an implosion (return the selected objects in the start position).

---
+++ Scripts/Implosion.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Leap.Unity;
+using Leap;
+
+public class Implosion : MonoBehaviour
+{
+  public RigidHand mano;
+  private bool afferraZoom = false;
+  private Dictionary<Transform, Vector3> localPosIniziali;
+
+  // Use this for initialization
+  void Start()
+  {
+    localPosIniziali = Utility.CalcoloPositionTransform();
+  }
+
+  public void OnValidate()
+  {
+    IHandModel ihm = this.gameObject.GetComponent<IHandModel>();
+
+    if (ihm != null)
+      mano = (RigidHand)ihm;
+  }
+
+  // Update is called once per frame
+  public void Update()
+  {
+    // L'utente deve fare una cosa per volta, o afferra o fa esplodere
+    if (!afferraZoom)
+    {
+      Controller cont = new Controller();
+
+      if (cont.IsConnected)
+      {
+        FingerModel dito = mano.fingers[1];
+        Ray raggio = new Ray(dito.GetBoneCenter(3), dito.GetBoneDirection(3));
+
+        mano.Implosion(localPosIniziali, raggio, "Imprendibile", "MainCamera");
+      }
+    }
+  }
+
+  private void StoAfferrando(bool stoAfferrando)
+  {
+    afferraZoom = stoAfferrando;
+  }
+
+  private void InZoom(bool zoom)
+  {
+    afferraZoom = zoom;
+  }
+}
