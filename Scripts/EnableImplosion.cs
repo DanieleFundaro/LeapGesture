33822d6c462c5d539daed46a0395ee327fd04022
Daniele Fundarò <daniele.fundaro@gmail.com>
2016-11-12 04:35:23 +0100

    EnableImplosion 1.0
    Button switch for enable the Implosion function.

---
+++ Scripts/EnableImplosion.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap.Unity;
+
+public class EnableImplosion : MonoBehaviour
+{
+  public void Start()
+  {
+    Utility.Enable<Implosion>(transform, null, false);
+  }
+
+  public void OnTriggerEnter(Collider other)
+  {
+    Utility.Enable<Implosion>(transform, other);
+  }
+}
