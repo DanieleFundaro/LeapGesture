f605b4fca7229cf074b470f1e8eaf91f013a2031
Daniele Fundarò <daniele.fundaro@gmail.com>
2016-11-14 17:56:08 +0100

    Utility 1.0.3
    Deleted the method "CalcoloLocalPositionTransform" to adapt to the
    script InitialPosition.

--- Scripts/Utility.cs
+++ Scripts/Utility.cs
@@ -19,37 +19,13 @@ public class Utility
     GameObject.Destroy(myLine, duration);
   }
 
-  public static Dictionary<Transform, Vector3> CalcoloLocalPositionTransform()
-  {
-    // Calcolo le posizioni iniziali di tutti gli oggetti presenti nella scena. All'occorrenza utilizzerò questi valori per effettuare qualche controllo
-    Dictionary<Transform, Vector3> localPosIniziali = new Dictionary<Transform, Vector3>();
-    Transform[] objs = SceneSettings.FindObjectsOfType<Transform>();
-
-    foreach (Transform obj in objs)
-    {
-      Transform p = obj.parent;
-      Vector3 dir = new Vector3(obj.position.x, obj.position.y, obj.position.z);
-
-      if (p != null)
-      {
-        dir.x -= p.position.x;
-        dir.y -= p.position.y;
-        dir.z -= p.position.z;
-      }
-
-      localPosIniziali.Add(obj, dir);
-    }
-
-    return localPosIniziali;
-  }
-
-  public static void Enable<T>(Transform obj, Collider other, bool startSetting = true) where T : Behaviour
+  public static void Enable<T>(Transform obj, Collider other, bool triggerSetting = true) where T : Behaviour
   {
     T b = obj.GetComponentInParent<T>();
 
     if (b != null)
     {
-      if (startSetting)
+      if (triggerSetting)
         if (other.GetComponentInParent<IHandModel>() != null)
           b.enabled = !b.enabled;
 
