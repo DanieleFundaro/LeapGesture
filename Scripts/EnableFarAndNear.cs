9a1976e15afe0118791826a829974ad1d607de31
Daniele Fundarò <daniele.fundaro@gmail.com>
2016-11-15 01:58:20 +0100

    EnableFarAndNear 1.2
    Now I active and deactive the graphic command, of the both hand, in the
    same time.

--- Scripts/EnableFarAndNear.cs
+++ Scripts/EnableFarAndNear.cs
@@ -11,5 +11,17 @@ public class EnableFarAndNear : MonoBehaviour
   public void OnTriggerEnter(Collider other)
   {
     Utility.Enable<FarAndNear>(transform, other);
+
+    Transform hm = GetComponentInParent<IHandModel>().transform.parent;
+    IHandModel[] mani = hm.GetComponentsInChildren<IHandModel>();
+
+    foreach (IHandModel ihm in mani)
+      if (ihm is RigidHand)
+      {
+        EnableFarAndNear efn = ihm.transform.GetChild(5).GetChild(0).GetComponentInChildren<EnableFarAndNear>();
+
+        if (efn != null)
+          efn.Start();
+      }
   }
 }
