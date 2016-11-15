83f0e74814c893fd5346c1853666f4309c09e5d2
Daniele Fundarò <daniele.fundaro@gmail.com>
2016-11-15 01:59:17 +0100

    EnableZoom 1.2
    Now I active and deactive the graphic command, of the both hand, in the
    same time.

--- Scripts/EnableZoom.cs
+++ Scripts/EnableZoom.cs
@@ -11,5 +11,17 @@ public class EnableZoom : MonoBehaviour
   public void OnTriggerEnter(Collider other)
   {
     Utility.Enable<Zoom>(transform, other);
+
+    Transform hm = GetComponentInParent<IHandModel>().transform.parent;
+    IHandModel[] mani = hm.GetComponentsInChildren<IHandModel>();
+
+    foreach (IHandModel ihm in mani)
+      if (ihm is RigidHand)
+      {
+        EnableZoom ez = ihm.transform.GetChild(5).GetChild(0).GetComponentInChildren<EnableZoom>();
+
+        if (ez != null)
+          ez.Start();
+      }
   }
 }
