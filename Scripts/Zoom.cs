7ea0271bdf625b594efb0bf5a98f96810260c9fc
Daniele Fundarò <daniele.fundaro@gmail.com>
2016-11-17 23:57:07 +0100

    Zoom 1.1
    Added new gesture to zooming.

--- Scripts/Zoom.cs
+++ Scripts/Zoom.cs
@@ -1,30 +1,48 @@
 ﻿using UnityEngine;
 using Leap.Unity;
+using Leap;
 
 public class Zoom : MonoBehaviour
 {
   public RigidHand manoDestra, manoSinistra;
   public float grabAngleMin = 2.6f, grabAngleMax = 2.8f, velocità = 10f;
-  private float offset = 0;
+  private float offset = 0, offset1 = 0;
 
   // Update is called once per frame
   void Update()
   {
     if (manoDestra.IsTracked && manoSinistra.IsTracked)
-      if (manoDestra.GetLeapHand().GrabAngle >= grabAngleMin && manoDestra.GetLeapHand().GrabAngle <= grabAngleMax  && manoSinistra.GetLeapHand().GrabAngle >= grabAngleMin && manoSinistra.GetLeapHand().GrabAngle <= grabAngleMax && manoDestra.GetPalmNormal().z / manoSinistra.GetPalmNormal().z < 0 && manoDestra.GetPalmDirection().x < 0 && manoSinistra.GetPalmDirection().x > 0)
+    {
+      Hand r = manoDestra.GetLeapHand(), l = manoSinistra.GetLeapHand();
+      Vector3 rn = manoDestra.GetPalmNormal(), rd = manoDestra.GetPalmDirection(), ln = manoSinistra.GetPalmNormal(), ld = manoSinistra.GetPalmDirection();
+
+      if (r.GrabAngle >= grabAngleMin && r.GrabAngle <= grabAngleMax && l.GrabAngle >= grabAngleMin && l.GrabAngle <= grabAngleMax && rn.z / ln.z < 0 && rd.x < 0 && ld.x > 0)
       {
         BroadcastMessage("InZoom", true);
-        Camera.main.fieldOfView = (offset + Distanza());
+        Camera.main.fieldOfView = (Distanza() - offset);
+      }
+      else if (r.GrabStrength == 0 && l.GrabStrength == 0 && rn.z < 0 && ln.z < 0 && rd.y > 0 && ld.y > 0)
+      {
+        BroadcastMessage("InZoom", true);
+        Camera.main.fieldOfView = Distanza1() - offset1;
       }
       else
       {
-        offset = Camera.main.fieldOfView - Distanza();
+        offset = Distanza() - Camera.main.fieldOfView;
+        offset1 = Distanza1() - Camera.main.fieldOfView;
         BroadcastMessage("InZoom", false);
       }
+    }
   }
 
   private float Distanza()
   {
     return (manoDestra.GetPalmPosition() - manoSinistra.GetPalmPosition()).magnitude * velocità;
+  }
+
+  private float Distanza1()
+  {
+    float offset = Mathf.Abs(manoDestra.GetPalmPosition().z) > Mathf.Abs(manoSinistra.GetPalmPosition().z) ? manoDestra.GetPalmPosition().z : manoSinistra.GetPalmPosition().z;
+    return offset * velocità;
   }
 }
