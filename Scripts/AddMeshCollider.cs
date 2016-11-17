c2805e50f7aca8595d4623ed301c45bd5641041b
Daniele Fundarò <daniele.fundaro@gmail.com>
2016-11-17 16:39:35 +0100

    AddMeshCollider 1.0
    Adds the MeshCollider component to the gameObject with a MeshRenderer
    (if doesn't have other type of collider).

---
+++ Scripts/AddMeshCollider.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddMeshCollider : MonoBehaviour
+{
+
+  // Use this for initialization
+  void Start()
+  {
+    MeshRenderer[] objsMeshRenderer = FindObjectsOfType<MeshRenderer>();
+
+    foreach (MeshRenderer obj in objsMeshRenderer)
+      if (obj.transform.tag != "Imprendibile" && obj.transform.tag != "MainCamera")
+      {
+        Collider c = obj.GetComponent<Collider>();
+
+        if (c == null)
+        {
+          obj.gameObject.AddComponent<MeshCollider>();
+          c = obj.GetComponent<MeshCollider>();
+          ((MeshCollider)c).convex = true;
+        }
+
+        c.isTrigger = true;
+      }
+  }
+}
