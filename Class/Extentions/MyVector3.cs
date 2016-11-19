0b084f32b55a8be3a57a3deddb3b653ad2af0632
Daniele Fundarò <daniele.fundaro@gmail.com>
2016-11-19 17:17:38 +0100

    MyVector3 1.0
    Moved the extension in the "Class/Extension" path.

---
+++ Class/Extentions/MyVector3.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityEngine
+{
+  public static class MyVector3
+  {
+    /// <summary>
+    /// Controlla se questo vettore è più lungo del vettore b, rispetto una direzione data dir. Verrà restituito in ogni caso in valore false, se questo vettore e la direzione non sono paralleli e coincidenti.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b">Vettore da paragonare.</param>
+    /// <returns></returns>
+    public static bool IsLongerThan(this Vector3 a, Vector3 b, Vector3 dir)
+    {
+      return a.normalized.Equals(dir.normalized) && a.magnitude >= b.magnitude;
+    }
+  }
+}
