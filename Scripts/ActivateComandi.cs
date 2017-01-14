a3fd4ef9a45de67e190af782cd41f377884c035e
Daniele Fundarò <daniele.fundaro@gmail.com>
2017-01-14 21:47:34 +0100

    ActivateComandi 1.0.1

--- Scripts/ActivateComandi.cs
+++ Scripts/ActivateComandi.cs
@@ -8,26 +8,17 @@ public class ActivateComandi : MonoBehaviour
   public RigidHand mano;
   public GameObject[] comandi;
 
-  public void OnValidate()
-  {
-    RigidHand rh = (RigidHand)this.GetComponent<IHandModel>();
-
-    if (rh != null)
-      mano = rh;
-  }
-
   // Update is called once per frame
   void Update()
   {
     Controller c = new Controller();
 
-    if (c.IsConnected)
+    if (mano.IsTracked)
     {
-      Hand h = c.Frame().Hand(mano.LeapID());
+      Hand h = mano.GetLeapHand();
 
-      if (h != null)
-        for (int i = 0; i < comandi.Length; i++)
-          comandi[i].SetActive(h.GrabStrength >= 0 && h.GrabStrength <= 0.1 && mano.GetPalmNormal().z < 0 && mano.GetPalmDirection().y > 0);
+      for (int i = 0; i < comandi.Length; i++)
+        comandi[i].SetActive(h.GrabStrength >= 0 && h.GrabStrength <= 0.1 && mano.GetPalmNormal().z < 0 && mano.GetPalmDirection().y > 0);
     }
   }
 }
